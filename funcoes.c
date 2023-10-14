e1ee6e5f5924758933d25968deed651ecdf4ada9
miqueiasvianasilva21 <miqueias@icomp.ufam.edu.br>
2023-10-13 22:31:11 -0400

    alterei tudo

--- funcoes.c
+++ funcoes.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "funcoes.h"
 double metrosParaKm(double metros) {
-    returna metros / 1000.0;
+    return metros / 1000.0;
 }
 
 double kmParaMetros(double km) {
-    returna km * 1000.0;
+    return km * 1000.0;
 }
 
 
